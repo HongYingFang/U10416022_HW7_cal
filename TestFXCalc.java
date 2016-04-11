20ba6d120a69fe874c5685ba464f70af72c95fb3
HongYingFang <zx04785@gmail.com>
2016-04-12 00:17:50 +0800

    Create TestFXCalc.java
    create a class to test FXCalc.class

---
+++ TestFXCalc.java
@@ -0,0 +1,25 @@
+//U10416022   
+import javafx.application.Application;
+import javafx.geometry.*;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+
+
+public class TestFXCalc{
+	//main method
+	public static void main(String[] args) {
+		
+			javafx.application.Application.launch(FXCalc.class);
+            	
+	}
+}
